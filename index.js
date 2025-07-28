2af5a35dcd0408528a8e4b16c13e2381d0a36e49
Yaskierix <124909610+Yaskierix@users.noreply.github.com>
2025-07-28 15:27:19 +0200

    Js changes

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import './style.scss';
 let myLeads = [];
 const inputEl = document.querySelector("#input-el");
 const inputBtn = document.querySelector("#input-btn");
+const ulEl = document.querySelector("#ul-el");
 
 inputBtn.addEventListener('click', () => {
   myLeads.push(inputEl.value);
+  console.log(myLeads);
 })
+
+for (let i = 0; i < myLeads.length; i++){
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
+
  
 
 
