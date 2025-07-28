2c458f15c4dd36eacfde7bb1870dc2af43a210fe
Yaskierix <124909610+Yaskierix@users.noreply.github.com>
2025-07-28 12:35:04 +0200

    JS setup

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import './style.scss';
 
+let myLeads = [];
+const inputEl = document.querySelector("#input-el");
+const inputBtn = document.querySelector("#input-btn");
 
+inputBtn.addEventListener('click', () => {
+  myLeads.push(inputEl.value);
+})
+ 
 
 
 
