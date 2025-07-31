2ccd3f7ed5c672cf4706c068281c1648888e066b
Yaskierix <124909610+Yaskierix@users.noreply.github.com>
2025-07-31 14:24:59 +0200

    Saving URL from current window and copying files on every build

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const inputBtn = document.querySelector("#input-btn");
 const ulEl = document.querySelector("#ul-el");
 const clearBtn = document.querySelector("#clear-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.querySelector("#tab-btn");
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
 
 function render(leads) {
   let listItems = "";
